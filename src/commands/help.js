5b68a4838cb4832380b8f9bda0576da375dfd09d
ent3r <32072697+ent3r@users.noreply.github.com>
2020-09-19 00:05:15 +0200

    Refactor basically everything

--- src/commands/help.js
+++ src/commands/help.js
@@ -1,17 +1,44 @@
+const { MessageEmbed } = require("discord.js");
+
 module.exports = {
-	name: "help",
-	description: "send the help message",
-	execute(client, message, Discord) {
-		const helpEmbed = new Discord.MessageEmbed() 
-			.setTitle('Command List')
-			.addField('t.register:', 'to set up your account use `t.register <https://t.me/(username)>`')
-			.addField('t.profile:', 'to call your profile link use `t.profile`, use `t.profile @JohnDoe#0000` to see if another user has a telegram account linked with the bot')
-			.addField('t.edit', 'to edit the telegram link associated with your account type `t.edit <https://t.me/(username)>`')
-			.addField('t.delete', 'to deregister your account from the database use `t.delete`')
-			.addField('t.source', 'to view the source code for the bot on github and report any issues you may be having')
-			.addField('Note:', 'this Bot is still in development and may have issues (visit https://t.me/telediscord for announcements)')
-			.setColor('0088cc')
-			.setFooter(`Bot made by GlenMerlin, currently serving ${client.guilds.cache.size} Server(s)`);
-		message.channel.send(helpEmbed);
-	},
+  name: "help",
+  description: "send the help message",
+  execute(client, message, args) {
+    if (!message.channel.permissionsFor(client.user).has("EMBED_LINKS")) {
+      return message.channel.send(
+        "Oops I don't have permission to embed messages, please contact the admins about this"
+      );
+    }
+    const helpEmbed = new MessageEmbed()
+      .setTitle("Command List")
+      .addField(
+        "t.register:",
+        "to set up your account use `t.register <https://t.me/(username)>`"
+      )
+      .addField(
+        "t.profile:",
+        "to call your profile link use `t.profile`, use `t.profile @JohnDoe#0000` to see if another user has a telegram account linked with the bot"
+      )
+      .addField(
+        "t.edit",
+        "to edit the telegram link associated with your account type `t.edit <https://t.me/(username)>`"
+      )
+      .addField(
+        "t.delete",
+        "to deregister your account from the database use `t.delete`"
+      )
+      .addField(
+        "t.source",
+        "to view the source code for the bot on github and report any issues you may be having"
+      )
+      .addField(
+        "Note:",
+        "this Bot is still in development and may have issues (visit https://t.me/telediscord for announcements)"
+      )
+      .setColor("0088cc")
+      .setFooter(
+        `Bot made by GlenMerlin, currently serving ${client.guilds.cache.size} Server(s)`
+      );
+    message.channel.send(helpEmbed);
+  },
 };
