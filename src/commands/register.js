3624218023b205de1df2cd655bfdd7abc610fdd0
GlenMerlin <glenthelucario@gmail.com>
2020-10-08 14:35:09 -0700

    Updated the handling of links
    Links are now upgraded to https and invalid links are filtered out instead of error 404-ing
    also updated the no user found response

--- src/commands/register.js
+++ src/commands/register.js
@@ -12,31 +12,35 @@ module.exports = {
 					"You are already registered in my database,\nif you wish to edit or remove yourself from the database please use the appropate commands (see **t.help** for more info)",
 				);
 			}
-			if (
-				message.content
-					.slice(11)
-					.match(/((?:http|https|)\/\/(?:t|telegram)\.me)/gi)
-			) {	
-				const URL = message.content.slice(11)
+			let URL = message.content.slice(11);
+			if (URL.match(/((?:http|https|)\/\/(?:t|telegram)\.me)/gi) || URL.match(/([!@#$%^&*(),.?":{}|<>])/gi)) {
+				// Checks if the link is using https, if not it upgrades the connection to https
+				if (!URL.match(/https/gi)) {
+					URL = "https" + URL.slice(4);
+					console.log(URL);
+				}	
 				// Send HTTP GET request with this URL, and make sure that this is a user
 				https.get(URL, (response) => {
 					response.on("data", (chunk) => {
 						// users that don't exist will always have meta name="robots" and channels will always have button saying "Preview channel"
 						if (chunk.toString().includes("robots", "Preview channel")) {
-							message.channel.send("This is not a telegram user.")
-						} else {
+							message.channel.send("hmm... I couldn't find any registered telegram users associated with that link, if you're new to telegram sign up at <https://web.telegram.org>");
+						}
+						else {
 							const addUserDB = new UserModel({
 								name: message.author.id,
-								link: message.content.slice(11).trim(),
+								link: URL,
 							});
 							addUserDB.save(function(err, addUserDB) {
 								if (err) return console.error(err);
 								message.channel.send("Congrats you registered successfully");
 							});
 						}
-					})
-				})
-			} else {
+					});
+				});
+			}
+			else {
+				console.log(err);
 				return message.channel.send(
 					"hmm... that doesn't seem to be a valid telegram link\nmake sure you formatted the command properly (check t.help to see proper formatting)",
 				);
