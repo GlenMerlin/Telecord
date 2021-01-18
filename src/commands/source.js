6ec0c7926ee683ab2ab551f7a7a44ae6c987eb10
GlenMerlin <GlenMerlin@protonmail.com>
2021-01-18 14:50:11 -0800

    Updated source.js to show proper github link

--- src/commands/source.js
+++ src/commands/source.js
@@ -3,7 +3,7 @@ module.exports = {
 	description: "Gives a message with the source code of the bot",
 	execute: (client, message, args) => {
 		message.channel.send(
-			"My source code is available at <https://github.com/GlenMerlin/Telegram-Discord-Bot>!",
+			"My source code is available at <https://github.com/GlenMerlin/Telecord>!",
 		);
 	},
 };
